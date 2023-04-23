5eb2600a69634a6f55fc32819a053b424aff184a
avillo12 <115428934+annavm12@users.noreply.github.com>
2023-04-23 17:26:39 +0200

    database

---
+++ app/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://teamrocket:pokemon@cluster0.ohi0qi5.mongodb.net/?retryWrites=true&w=majority',
+    MONGODB_OPTIONS: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  };
+
