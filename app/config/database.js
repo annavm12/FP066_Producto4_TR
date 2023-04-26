ea8e818ff80b3347fc84b8c406b21b0aa6f312d5
dacaro12 <dav.cat.roc@gmail.com>
2023-04-26 20:16:47 +0200

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const config = require("./config");
 
 // const MONGODB_URI = 'mongodb://¨{config.MONGODB_DATABASE}';
-const MONGODB_URI= 'mongodb+srv://teamrocket:pokemon@cluster0.ohi0qi5.mongodb.net/?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://teamrocket:pokemon@cluster0.ohi0qi5.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose
-    .connect(MONGODB_URI, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnfiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
- })
- .then((db) => console.log("Mongodb is connected to", db.connection.host))
- .catch ((err) => console.error (err));
+  })
+  .then((db) => console.log("Mongodb is connected to", db.connection.host))
+  .catch((err) => console.error(err));
