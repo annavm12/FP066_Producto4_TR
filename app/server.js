13e4e4a651abbb4e02269b15be82420f7eccc4ea
avillo12 <115428934+annavm12@users.noreply.github.com>
2023-04-26 20:07:33 +0200

    1212

--- app/server.js
+++ app/server.js
@@ -1,199 +1,43 @@
-/*const express = require('express');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const { ApolloServer, gql } = require('apollo-server-express');
 
-const { PORT, GRAPHQL_PATH } = require('./config/config');
-const { MONGODB_URI, MONGODB_OPTIONS } = require('./config/database');
+// Conectar a la base de datos de MongoDB
+mongoose.connect('mongodb+srv://teamrocket:pokemon@cluster0.ohi0qi5.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((error) => console.log('Error al conectar a la base de datos:', error));
 
-const semanaController = require('./controllers/semanacontroller');
-const tareaController = require('./controllers/tareacontroller');
+// Definir un esquema de GraphQL
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
 
-const Semana = require('./models/semana');
-const Tarea = require('./models/tarea');
-const config = require('./config/config');
-
-
-//conexion
-mongoose.connect(MONGODB_URI, MONGODB_OPTIONS);
-
-
-
-//Definicion schemes GraphQL
-const typeDefs = gql`
-    type Semana {
-        id: ID!
-        semana: Int!
-        anio: Int!
-        descripcion: String!
-        mes: String!
-        horas: Int!
-        color: String!
-    }
-    
-    input NuevaSemanaInput {
-        semana: Int!
-        anio: Int!
-        descripcion: String!
-        mes: String!
-        horas: Int!
-        color: String!
-    }
-    
-    input ActualizarSemanaInput {
-        id: ID!
-        semana: Int
-        anio: Int
-        descripcion: String
-        mes: String
-        horas: Int
-        color: String
-    }
-    
-    type Query {
-        obtenerSemana(id: ID!): Semana
-        obtenerSemanas: [Semana!]!
-    }
-    
-    type Mutation {
-        nuevaSemana(input: NuevaSemanaInput): Semana
-        actualizarSemana(input: ActualizarSemanaInput): Semana
-        eliminarSemana(id: ID!): ID
-    }
-    type Tarea {
-        id: ID!
-        nombre: String!
-        horaInicio: String!
-        horaFinal: String!
-        descripcion: String!
-        colaboradores: String!
-        prioridad: String!
-        complete: Boolean!
-      }
-      
-      input NuevaTareaInput {
-        nombre: String!
-        horaInicio: String!
-        horaFinal: String!
-        descripcion: String!
-        colaboradores: String!
-        prioridad: String!
-        complete: Boolean
-        contenedorId: ID!
-      }
-      
-      input ActualizarTareaInput {
-        id: ID!
-        nombre: String
-        horaInicio: String
-        horaFinal: String
-        descripcion: String
-        colaboradores: String
-        prioridad: String
-        complete: Boolean
-        contenedorId: ID
-      }
-      
-      type Query {
-        obtenerTarea(id: ID!): Tarea
-        obtenerTareas: [Tarea!]!
-      }
-      
-      type Mutation {
-        nuevaTarea(input: NuevaTareaInput): Tarea
-        actualizarTarea(input: ActualizarTareaInput): Tarea
-        eliminarTarea(id: ID!): ID
-      }
-      `;      
-
-//esto es del producto 3, pero me recomendaban hacerlo ahora... Lo dejo ya realiazado
+// Definir las resolvers para el esquema
 const resolvers = {
   Query: {
-    semanas: async () => {
-      try {
-        const semanas = await Semana.find();
-        return semanas;
-      } catch (err) {
-        throw new Error(`Error al obtener las semanas: ${err}`);
-      }
-    },
-    tareas: async () => {
-      try {
-        const tareas = await Tarea.find().populate('contenedor');
-        return tareas;
-      } catch (err) {
-        throw new Error(`Error al obtener las tareas: ${err}`);
-      }
-    }
-  },
-  Mutation: {
-    crearSemana: async (_, { semanaInput }) => {
-      try {
-        const semana = new Semana({
-          semana: semanaInput.semana,
-          anio: semanaInput.anio,
-          descripcion: semanaInput.descripcion,
-          mes: semanaInput.mes,
-          horas: semanaInput.horas,
-          color: semanaInput.color
-        });
-        const nuevaSemana = await semana.save();
-        return nuevaSemana;
-      } catch (err) {
-        throw new Error(`Error al crear una nueva semana: ${err}`);
-      }
-    },
-    eliminarSemana: async (_, { id }) => {
-      try {
-        const semanaEliminada = await Semana.findByIdAndRemove(id);
-        return semanaEliminada;
-      } catch (err) {
-        throw new Error(`Error al eliminar la semana: ${err}`);
-      }
-    },
-    crearTarea: async (_, { tareaInput }) => {
-      try {
-        const tarea = new Tarea({
-          nombre: tareaInput.nombre,
-          horaInicio: tareaInput.horaInicio,
-          horaFinal: tareaInput.horaFinal,
-          descripcion: tareaInput.descripcion,
-          colaboradores: tareaInput.colaboradores,
-          prioridad: tareaInput.prioridad,
-          complete: tareaInput.complete,
-          contenedor: tareaInput.contenedor
-        });
-        const nuevaTarea = await tarea.save();
-        return nuevaTarea;
-      } catch (err) {
-        throw new Error(`Error al crear una nueva tarea: ${err}`);
-      }
-    },
-    eliminarTarea: async (_, { id }) => {
-      try {
-        const tareaEliminada = await Tarea.findByIdAndRemove(id);
-        return tareaEliminada;
-      } catch (err) {
-        throw new Error(`Error al eliminar la tarea: ${err}`);
-      }
-    }
+    hello: () => 'Hola mundo!'
   }
-};*/
+};
 
+// Crear un servidor Apollo
+const server = new ApolloServer({ typeDefs, resolvers });
 
-const server = new ApolloServer({ typeDefs, resolvers});
+// Crear una instancia de Express
+const app = express();
 
-const { url } =  startStandaloneServer(server, {
+// Iniciar el servidor Apollo y conectarlo con Express
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
-  listen: { port: config.PORT },
+startServer();
 
-
-}).then(( data)=>console.log(data));
-
-
-console.log(`🚀  Server ready at: ${url}`);
-
-
-
-
+// Iniciar el servidor Express
+app.listen(3000, () => console.log('Servidor iniciado'));
 
