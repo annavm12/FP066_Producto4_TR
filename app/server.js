37515d8bd98da0d0e065ff2440ccf4542cf63bbe
unknown <avillo@uoc.edu>
2023-05-07 19:18:58 +0200

    first commit p3

--- app/server.js
+++ app/server.js
@@ -10,25 +10,165 @@ mongoose.connect('mongodb+srv://teamrocket:pokemon@cluster0.ohi0qi5.mongodb.net/
   .then(() => console.log('Conectado a la base de datos'))
   .catch((error) => console.log('Error al conectar a la base de datos:', error));
 
-// Definir un esquema de GraphQL
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+  const Semana = require('./models/semana');
+  const Tarea = require('./models/tarea');
+  const semanaController = require('./controllers/semanacontroller');
+  const tareaController = require ('./controllers/tareacontroller');
 
-// Definir las resolvers para el esquema
+  // Crear una instancia de Express
+    const app = express();
+
+  app.get('/semana',semanaController.obtenerSemanas);
+  app.post('/semana',semanaController.crearSemana);
+  app.put('/semana/:id', semanaController.actualizarSemana);
+  app.delete('/semana/:id', semanaController.eliminarSemana);
+
+  app.get('tarea', tareaController.obtenerTarea);
+  app.post('tarea', tareaController.crearTarea);
+
+
+ const typeDefs = `
+  type Semana {
+    _id: ID!
+    semana: Int!
+    anio: Int!
+    descripcion: String!
+    mes: String!
+    horas: Int!
+    color: String!
+  }
+    type Tarea {
+      nombre: String!
+      horaInicio: String!
+      horaFinal: String!
+      descripcion: String!
+      colaboradores: String!
+      prioridad: String!
+      complete: Boolean
+    }
+    
+
+  input NuevaSemanaInput {
+    semana: Int!
+    anio: Int!
+    descripcion: String!
+    mes: String!
+    horas: Int!
+    color: String!
+}
+
+  input ActualizarSemanaInput {
+      id: ID!
+      semana: Int
+      anio: Int
+      descripcion: String
+      mes: String
+      horas: Int
+      color: String
+  }
+  input NuevaTareaInput {
+    nombre: String!
+    horaInicio: String!
+    horaFinal: String!
+    descripcion: String!
+    colaboradores: String!
+    prioridad: String!
+    complete: Boolean
+    contenedorId: ID!
+  }
+
+  input ActualizarTareaInput {
+    id: ID!
+    nombre: String
+    horaInicio: String
+    horaFinal: String
+    descripcion: String
+    colaboradores: String
+    prioridad: String
+    complete: Boolean
+    contenedorId: ID
+}
+type Query {
+  obtenerSemana(id: ID!): Semana
+  obtenerSemanas: [Semana!]!
+}
+type Query {
+  obtenerTarea(id: ID!): Tarea
+  obtenerTareas: [Tarea!]!
+}
+type Mutation {
+  nuevaSemana(input: NuevaSemanaInput): Semana
+  actualizarSemana(input: ActualizarSemanaInput): Semana
+  eliminarSemana(id: ID!): ID
+}
+type Mutation {
+  nuevaTarea(input: NuevaTareaInput): Tarea
+  actualizarTarea(input: ActualizarTareaInput): Tarea
+  eliminarTarea(id: ID!): ID
+}
+`;
+//resolvers
+
+
 const resolvers = {
   Query: {
-    hello: () => 'Hola mundo!'
-  }
+    obtenerSemana: async (_, { id }) => {
+      const semana = await Semana.findById(id);
+      return semana;
+    },
+    obtenerSemanas: async () => {
+      const semanas = await Semana.find();
+      return semanas;
+    },
+    obtenerTarea: async (_, { id }) => {
+      const tarea = await Tarea.findById(id);
+      return tarea;
+    },
+    obtenerTareas: async () => {
+      const tareas = await Tarea.find();
+      return tareas;
+    }
+  },
+  Mutation: {
+    nuevaSemana: async (_, { input }) => {
+      const semana = new Semana(input);
+      await semana.save();
+      return semana;
+    },
+    actualizarSemana: async (_, { input }) => {
+      const { id, ...actualizaciones } = input;
+      const semana = await Semana.findByIdAndUpdate(id, actualizaciones, { new: true });
+      return semana;
+    },
+    eliminarSemana: async (_, { id }) => {
+      await Semana.findByIdAndDelete(id);
+      return id;
+    },
+    nuevaTarea: async (_, { input }) => {
+      const tarea = new Tarea(input);
+      await tarea.save();
+      return tarea;
+    },
+    actualizarTarea: async (_, { input }) => {
+      const { id, ...actualizaciones } = input;
+      const tarea = await Tarea.findByIdAndUpdate(id, actualizaciones, { new: true });
+      return tarea;
+    },
+    eliminarTarea: async (_, { id }) => {
+      await Tarea.findByIdAndDelete(id);
+      return id;
+    }
+  },
+ 
 };
+
+module.exports = resolvers;
+
 
 // Crear un servidor Apollo
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// Crear una instancia de Express
-const app = express();
+
 
 // Iniciar el servidor Apollo y conectarlo con Express
 async function startServer() {
